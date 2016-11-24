783c3f7c37071fb7255f937a7a3e4828ff618cc8
Evan Wappel <ewappel@gmail.com>
2016-11-24 07:36:42 -0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,10 +5,10 @@ echo "starting script..."
 
 
 echo "update ubuntu here..."
-sudo apt-get update &&
-sudo apt-get -y upgrade &&
-sudo apt -y install python-pip &&
-sudo pip install --upgrade pip
+echo sudo apt-get update
+#sudo apt-get -y upgrade &&
+#sudo apt -y install python-pip &&
+#sudo pip install --upgrade pip
 
 
 
